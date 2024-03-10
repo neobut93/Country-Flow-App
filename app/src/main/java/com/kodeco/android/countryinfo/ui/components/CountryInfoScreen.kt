b22e0b51db234aa2f5551eca3b8b80080e8918fc
vadim <neobut93@gmail.com>
2024-03-10 16:21:48 -0700

    removed typo

--- app/src/main/java/com/kodeco/android/countryinfo/ui/components/CountryInfoScreen.kt
+++ app/src/main/java/com/kodeco/android/countryinfo/ui/components/CountryInfoScreen.kt
@@ -15,7 +15,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
-t
+
 sealed class CountryInfoState {
     object Loading : CountryInfoState()
     data class Success(val countries: List<Country>) : CountryInfoState()
