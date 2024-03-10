8c1d229fab942321706bc9cbfedee12d5391b51c
vadim <neobut93@gmail.com>
2024-03-10 16:27:50 -0700

    removed unused imports from Loading.kt

--- app/src/main/java/com/kodeco/android/countryinfo/ui/components/Loading.kt
+++ app/src/main/java/com/kodeco/android/countryinfo/ui/components/Loading.kt
@@ -1,7 +1,5 @@
 package com.kodeco.android.countryinfo.ui.components
 
-import android.annotation.SuppressLint
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,15 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.kodeco.android.countryinfo.flow.Flows
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
-import kotlin.concurrent.timer
 
-@SuppressLint("CoroutineCreationDuringComposition", "StateFlowValueCalledInComposition")
 @Composable
 fun Loading() {
     val counter by Flows.counterFlow.collectAsState()
@@ -31,7 +21,6 @@ fun Loading() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
-        // TODO: Show the current Flows.counterFlow value here in Text composable
         Text("Current Count: $counter")
         CircularProgressIndicator()
     }
